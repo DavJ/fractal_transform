08dcc598bbdfa7b617b02ad209444328dc0a5b13
David Jaros <jdavid.cz@gmail.com>
2023-11-28 23:35:58 +0100

    progress

--- fractal-transform.py
+++ fractal-transform.py
@@ -1,7 +1,10 @@
+from typing import List
 import cmath
 import math
+import numpy as np
 
-PI = math.pi()
+PI = math.pi
+j=1j
 
 def fractal_transform1(signal: List[complex], k_sigma: complex):
     order = len(signal)
@@ -12,13 +15,17 @@ def fractal_transform1(signal: List[complex], k_sigma: complex):
     else: 
        signal_even = signal[0:order:2]
        signal_odd = signal[1:order:2]
-       W = exp(-j*2*PI*k_sigma/order) 
+       W = cmath.exp(-j*2*PI*k_sigma/order)
 
        return fractal_transform1(signal_even, k_sigma) + W*fractal_transform1(signal_odd, k_sigma)
 
-def fractal_transform(signal: List[complex], K:int , L: int):
+def fractal_transform(signal: List[complex]):
+    K = len(signal)
+    L = K
+    return np.array([[fractal_transform1(signal, k*cmath.exp(j*2*PI*l/L)) for k in range(K)] for l in range(L)])
 
-    return [fractal_transform1(signal, k*exp(j*theta)) for k in range(K)] for theta in range(L)]
+print(fractal_transform([1,0,1,0,1,0,1,0]))
+
     
      
 
